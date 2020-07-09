8bbb41b87532b463c3e96a5ea460ce952dd62529
Anushree Soman <ANUSHREE.SOMAN@BARUCHMAIL.CUNY.EDU>
2020-07-09 16:06:50 -0400

    added card-match function

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach((card) => card.addEventListener("click", flipCard));
 
-function flipCard(){
-    this.classList.toggle('flip');
+let cardFlipped = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    
+  if (lockBoard) return;
+
+  this.classList.add("flip");
+
+  if (!cardFlipped) {
+    //Store first card flipped
+    cardFlipped = true;
+    firstCard = this;
+
+    return;
+  }
+
+  //Store second card flipped
+  cardFlipped = false;
+  secondCard = this;
+
+  checkMatched();
+}
+
+function checkMatched() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  //if cards match prevent them being clicked again
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1200);
+}
+
+function resetBoard() {
+  [cardFlipped, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
