ff2af8b328e0b778e6b74693ca21857b4c8a0f85
Anushree Soman <ANUSHREE.SOMAN@BARUCHMAIL.CUNY.EDU>
2020-07-31 18:37:46 -0400

    Added countdown and counter

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ let lockBoard = false;
 let firstCard, secondCard;
 let combosFound = 0;
 
+//Game play
 function flipCard() {
-  if(this === firstCard) return; //prevents same card being clicked twice
-  
+  if (this === firstCard) return; //prevents same card being clicked twice
+
   if (lockBoard) return;
 
   this.classList.add("flip");
+
+  cardFlips += 1;
+  flipCount.innerHTML = `${cardFlips}`;
 
   if (!cardFlipped) {
     //Store first card flipped
@@ -40,9 +44,9 @@ function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
 
-  combosFound +=1;
-  
-  if (combosFound === 8){
+  combosFound += 1;
+
+  if (combosFound === 8) {
     winGame();
   }
 }
@@ -63,23 +67,81 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
-function winGame(){
-  setTimeout(() => {
-    alert('You Win!');
-    cards.forEach(card => {card.classList.remove('flip'); card.addEventListener("click", flipCard)});
-    shuffle();
-  }, 1200);
-}
-
 function shuffle() {
-  cards.forEach(card => {
-    let posit = Math.floor(Math.random() *12);
+  cards.forEach((card) => {
+    let posit = Math.floor(Math.random() * 12);
     card.style.order = posit;
-  })
+  });
 }
 
 (function loaded() {
   shuffle();
 })();
 
+//Flip Counter for cards
+const flipCount = document.getElementById("flip-count");
+let cardFlips = 0;
 
+//Timer for game
+const countdown = document.getElementById("countdown");
+
+const startingMin = 5;
+let time = startingMin * 60;
+
+function updateCountdown() {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  countdown.innerHTML = `${minutes}:${seconds}`;
+  if (minutes === 0 && seconds == 00 && combosFound !== 8) {
+    countdown.innerHTML = `0:00`;
+    resetCountdown();
+    setTimeout(() => {
+      //console.log("Now its 0");
+      loseGame();
+    }, 1000);
+  } else if (minutes >= 0) {
+    time--;
+  }
+}
+setInterval(updateCountdown, 1000);
+
+//Win-loss decision and game reset
+function winGame() {
+  setTimeout(() => {
+    shuffle();
+    alert("You Win!");
+    resetCounter();
+    cards.forEach((card) => {
+      card.classList.remove("flip");
+      card.addEventListener("click", flipCard);
+    });
+  }, 1000);
+  resetCountdown();
+}
+
+function resetCountdown() {
+  time = startingMin * 60;
+  setTimeout(() => {
+    updateCountdown();
+  }, 1000);
+}
+
+function resetCounter() {
+  cardFlips = 0;
+  flipCount.innerHTML = `${cardFlips}`;
+}
+
+function loseGame() {
+  alert("Time is up!");
+  resetCounter();
+  cards.forEach((card) => {
+    card.classList.remove("flip");
+    card.addEventListener("click", flipCard);
+  });
+  setTimeout(() => {
+    shuffle();
+  }, 1000);
+}
